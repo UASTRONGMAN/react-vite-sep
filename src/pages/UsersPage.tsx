274deb85755c32994bd2725cf13181fab6500e36
UASTRONGMAN <shkolniy.maks@ukr.net>
2025-01-13 01:45:24 +0200

    hw3.3

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,12 +1,9 @@
 import Users from "../components/users/Users.tsx";
-import {Outlet} from "react-router-dom";
 
 const UsersPage = () => {
 
     return (
         <div>
-            <Outlet/>
-            <hr/>
             <Users/>
         </div>
     );
